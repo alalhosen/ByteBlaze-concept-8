651eb44c8be71b07340a38cf9f970b386a5a760a
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-17 23:57:40 +0600

    Link to btn in hero section

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -2,7 +2,7 @@ import Hero from "../../components/Hero";
 
 const Home = () => {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center min-h-[calc(100vh-116px)]">
             <Hero />
         </div>
     );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,25 @@
 
 const Hero = () => {
-    return (
-        <div className="hero bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Welcome to BytrBlaze</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-              quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-            <button className="btn btn-primary">Read Blogs</button>
-            <button className="btn btn-primary">Bookmarks</button>
+  return (
+    <div className="hero bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-lg">
+          <h1 className="text-5xl font-bold">Welcome to <span>BytrBlaze</span></h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+            quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p>
+          <div className="flex gap-2 justify-center">
+            <Link to='/blogs'></Link>
+            <Link to='bookmarks'></Link>
+
           </div>
+          <button className="btn btn-primary">Read Blogs</button>
+          <button className="btn btn-primary">Bookmarks</button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Hero;
