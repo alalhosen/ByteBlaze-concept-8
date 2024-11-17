9eed4a7b605a2087c597f724648105d2457a9b86
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-17 21:15:01 +0600

    children component in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,29 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Blogs from './assets/pages/Blogs.jsx'
 import Bookmarks from './assets/pages/Bookmarks';
 import MainLayout from './layouts/MainLayout.jsx'
+import Home from './assets/Pages/Home.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <MainLayout/>
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/blogs',
+        element: <Blogs />
+      },
+      {
+        path: '/bookmarks',
+        element: <Bookmarks />
+      }
+
+    ]
   },
-  {
-    path: '/blogs',
-    element: <Blogs />
-  },
-  {
-    path: '/bookmarks',
-    element: <Bookmarks/>
-  }
+
 ])
 
 createRoot(document.getElementById('root')).render(
