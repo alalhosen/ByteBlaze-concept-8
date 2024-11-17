bac6a5b2b423e79b8e40146c0d720d162463c263
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-17 22:29:07 +0600

    two btn in banner section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,13 @@ const Hero = () => {
         <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
+            <h1 className="text-5xl font-bold">Welcome to BytrBlaze</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
               quasi. In deleniti eaque aut repudiandae et a id nisi.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn btn-primary">Read Blogs</button>
+            <button className="btn btn-primary">Bookmarks</button>
           </div>
         </div>
       </div>
