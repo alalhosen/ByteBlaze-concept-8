3d0ac6b76775b04622812b2e0f80a56cc139eca8
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-18 22:52:45 +0600

    waveTop.svg set

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import Hero from "../components/Hero";
 import wave from "../assets/wave.svg"
+import waveTop from "../assets/waveTop.svg"
 const Home = () => {
     return (
+        
         <div className="flex flex-col relative items-center justify-center min-h-[calc(100vh-116px)]">
+            <img className="absolute top-0 h-1/3 w-full"  src={waveTop} alt="" />
             <Hero />
-        <img className="absolute bottom-0 w-full" src={wave} alt="" />
+        <img className="absolute bottom-0 w-full h-2/5" src={wave} alt="" />
         </div>
     );
 };
