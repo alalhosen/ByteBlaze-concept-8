5214eba8947944a6469587b6a4da9aaca33bf9cb
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-18 21:07:00 +0600

    wave.svg set in under the banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ import Hero from "../components/Hero";
 import wave from "../assets/wave.svg"
 const Home = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-[calc(100vh-116px)]">
+        <div className="flex flex-col relative items-center justify-center min-h-[calc(100vh-116px)]">
             <Hero />
-        <img src={wave} alt="" />
+        <img className="absolute bottom-0 w-full" src={wave} alt="" />
         </div>
     );
 };
