61dee1dde39a2e200a6f0e21dd3e73bf90391d09
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-19 23:23:53 +0600

    fetch set

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,5 +1,14 @@
+import { useEffect, useState } from "react";
 
 const Blogs = () => {
+    
+    const [blogs,setBlogs]=useState([])
+    useEffect(()=>{
+        fetch('https://dev.to/api/articles?per_page=9&top=7')
+        .then(res=>res.json())
+        .then(data=>setBlogs(data))
+    },[])
+    console.log(blogs);
     return (
 <section className="dark:bg-gray-100 dark:text-gray-800">
 	<div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
