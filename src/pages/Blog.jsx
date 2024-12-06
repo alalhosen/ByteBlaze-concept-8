98491d97e8f246631e2481250b0798afb871bd3a
Alal Hosen <alalhosen1992620120@gmail.com>
2024-12-06 19:59:25 +0600

    placeholderImage add

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -8,53 +8,50 @@ const Blog = () => {
         comments_count, title, reading_time_minutes, public_reactions_count, published_at
     } = blog
     return (
-        <div className="max-w-3xl px-6 py-16 mx-auto space-y-12">
+        <div className="space-y-12 mx-auto px-6 py-16 max-w-3xl">
             <article className="space-y-8 dark:bg-gray-100 dark:text-gray-900">
                 <div className="space-y-6">
-                    <h1 className="text-4xl font-bold md:tracking-tight md:text-5xl">{title}</h1>
-                    <div className="flex flex-col items-start justify-between w-full md:flex-row md:items-center dark:text-gray-600">
+                    <h1 className="font-bold text-4xl md:text-5xl md:tracking-tight">{title}</h1>
+                    <div className="flex md:flex-row flex-col justify-between items-start md:items-center w-full dark:text-gray-600">
 
                         <p className="text-sm">{reading_time_minutes} min read • {new Date(published_at).toLocaleDateString()}</p>
 
-                        <p className="flex-shrink-0 mt-3 text-sm md:mt-0">
+                        <p className="flex-shrink-0 mt-3 md:mt-0 text-sm">
                             {comments_count} comments • {public_reactions_count} views</p>
                     </div>
 
                     {/* Tabs */}
 
-                    <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap dark:bg-gray-100 dark:text-gray-800">
+                    <div className="flex flex-nowrap sm:justify-start items-center dark:bg-gray-100 -mx-4 dark:text-gray-800 overflow-x-auto overflow-y-hidden">
 
                         <Link to=''
-                            className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600">
+                            className="flex flex-shrink-0 items-center space-x-2 dark:border-gray-600 px-5 py-3 border-b dark:text-gray-600">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                                 <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                             </svg>
                             <span>Content</span>
                         </Link>
                         <Link to={`author`} rel="noopener noreferrer" href="#"
-                            className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg dark:border-gray-600 dark:text-gray-900">
+                            className="flex flex-shrink-0 items-center space-x-2 dark:border-gray-600 px-5 py-3 border border-b-0 rounded-t-lg dark:text-gray-900">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                                 <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                                 <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
                             </svg>
                             <span>Author</span>
                         </Link>
-
                     </div>
-
                 </div>
             </article>
 
-
             <div>
-                <div className="flex flex-wrap py-6 gap-2 border-t border-dashed dark:border-gray-600">
-                    <a rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50">#MambaUI</a>
-                    <a rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50">#TailwindCSS</a>
-                    <a rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50">#Angular</a>
+                <div className="flex flex-wrap gap-2 dark:border-gray-600 py-6 border-t border-dashed">
+                    <a rel="noopener noreferrer" href="#" className="dark:bg-violet-600 px-3 py-1 rounded-sm dark:text-gray-50 hover:underline">#MambaUI</a>
+                    <a rel="noopener noreferrer" href="#" className="dark:bg-violet-600 px-3 py-1 rounded-sm dark:text-gray-50 hover:underline">#TailwindCSS</a>
+                    <a rel="noopener noreferrer" href="#" className="dark:bg-violet-600 px-3 py-1 rounded-sm dark:text-gray-50 hover:underline">#Angular</a>
                 </div>
                 <div className="space-y-2">
-                    <h4 className="text-lg font-semibold">Related posts</h4>
-                    <ul className="ml-4 space-y-1 list-disc">
+                    <h4 className="font-semibold text-lg">Related posts</h4>
+                    <ul className="space-y-1 ml-4 list-disc">
                         <li>
                             <a rel="noopener noreferrer" href="#" className="hover:underline">Nunc id magna mollis</a>
                         </li>
